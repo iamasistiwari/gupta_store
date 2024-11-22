64644e15116fc58ce4198f6ddba34ba1940f46d3
iamasistiwari <iamashishtiwari2004@gmail.com>
2024-11-22 20:04:46 +0530

    v1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Manrope } from "next/font/google";
-import Providers from "./providers";
 
 const manrope = Manrope({
   subsets: ["latin"],
@@ -20,11 +19,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html suppressHydrationWarning lang="en" className={manrope.variable}>
+    <html lang="en" className={manrope.variable}>
       <body className={`${manrope.className} antialiased `}>
-        <Providers>
           {children}
-        </Providers>
       </body>
     </html>
   );
