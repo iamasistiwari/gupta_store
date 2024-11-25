f1385709ac7e66057fe5637b76ecf67b590c9dd0
AryanMahajan <mahajanaryan0409@gmail.com>
2024-11-25 10:45:10 +0530

    signup page added

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,20 @@
+import Footer from '@/components/main/Footer'
+import HeroSection from '@/components/main/HeroSection'
+import Navbar from '@/components/main/Navbar'
+import React from 'react'
+
+export default function page() {
+  return (
+    <>
+      <header className='bg-[#0c6c44]'>
+        <Navbar />
+      </header>
+      <main className='w-screen'>
+        <HeroSection />
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </>
+  )
+}
